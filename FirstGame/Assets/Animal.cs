38ba8629a2e2b43e56c3930865a771c1f111c5d5
Unknown <mickailahinnen@gmail.com>
2018-01-18 11:15:28 -0700

    Added animal class. Added notes, made a sphere. Click for meow

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	// This declares what start will do
+	void Start () {
+		print("my animal is eating.");
+		print("Now my animal is sleeping");
+		//print does not exsist with a capital P, it must be lower case.
+		//idicates notes, and indicates for the computer to ignore it.
+		// this () indicates a function, must have "" for a string of txt.
+		// this {} is an object.
+		// print is a function call.
+		// this ; is a period, ends a line of txt.
+		print("I love mt cat.");
+	}
+	void OnMouseDown()
+	{
+		print("meow");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- FirstGame/Assets/SecondLevel.cs
+++ FirstGame/Assets/SecondLevel.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Class is an object classification
+//You must have and closed per object
+//void is a function that is empty or null
 public class SecondLevel : MonoBehaviour {
 
 	// Use this for initialization
