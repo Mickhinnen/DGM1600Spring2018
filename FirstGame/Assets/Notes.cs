661c39cfc4253bf79c705f4de4385287157f5ada
Unknown <mickailahinnen@gmail.com>
2018-01-25 16:44:34 -0700

    Variable notes for homework

--- FirstGame/Assets/Notes.cs
+++ FirstGame/Assets/Notes.cs
@@ -21,3 +21,14 @@ of data, String, Int, and Float are a class*/
  
  /* using UnityEngine.UI.. is a folder with classes in it, another box.
  UI is text box on screen of game. */
+ 
+ /* Variable in english is a thing that can change. I believe it is the same in c#, however 
+ we as the scripters are the ones instigating that change in the variables. Today we focused on
+ three of them. Int, Float, and String. Int, short for integer is, for math geniouses, a 
+ whole number, this stays true in C#. This can be seen in codeing with things such as Foodcounter
+ which we were able to change or if left blank is at 0. Float is also a number type data but in
+ other numbers in decimal, numbers such as 1.00045, etc. While this can help it takes up more space
+ as it uses more characters. I can see this being used in counters like racing games for a 
+ speedomoter, or a points system. String is a word variable. Pieces of lingual data strung together.
+ We used this with imputing a name on our objects and having it reference, print text for actions
+ like on start or onmousedown. And thats about the extent of my understanding. */
