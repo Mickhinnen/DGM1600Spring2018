1007036fd14d7ed861d7403a011e91c5ab2d965e
Unknown <mickailahinnen@gmail.com>
2018-02-01 21:18:26 -0700

    Else Clause homework in Assignments

--- FirstGame/Assets/Assighnments.cs
+++ FirstGame/Assets/Assighnments.cs
@@ -26,3 +26,17 @@ automaticaly false. then you can script if ( key == false); { print("the door is
 if (key == true); { print("the door is open"); } This can be toggled in the game to open and close
 a door, with more coding this can turn off and on a light switch, decided what an object says if 
 the situation matches and so on. Either way I like If statements. */
+
+/* Else Clause Homework
+ The else clause works with if statements really turning it to a two option code. Going back to 
+ the example of using a name we can change if (Name == cat); { print("meow"); } so that either way
+ there is an action taking by scripting it as if( "Name == cat"); { print("meow"); } else { 
+ print("bark"); } making it so that any object but cat will bark when called. With the foodcounter
+ again it expanse the code, if(Foodcounter > 0); { print("my cat has food"); } else { print("My cat
+ doesnt have food"); } This can end a game if useing health, after adding public int Health = 100,
+ write if(Health > 0); { print("Game Start"); } else { print("Game Over") } or it can be used for 
+ a passwor by adding public string Password = "Gotta Catch Them All" or whatever you want as a password
+ then add if(Password == "Gotta Catch Them All") { print("Open"); } else { print("No"); } Thoughwhats 
+ neat to is that all this can be shortend into if(key ? "Door unlocked" : "Door is locked"); or such
+ however as fun as if or else is its really only best to use it for one or the other situations as
+ putting in multiple options this way can get really long.    */
