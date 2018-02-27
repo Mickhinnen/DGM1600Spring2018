479e4e7b0d96fdab95b9f319166c0ef86517d0d3
Unknown <mickailahinnen@gmail.com>
2018-02-27 10:05:22 -0700

    movement still not working

---
+++ MyOwnTime/Test/Assets/scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+public float speed = 4f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Imput.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+    var z = Imput.GetAzis("Vertical") * Time.deltaTime * 3.0f
+    
+    transform.Rotate(0, x, 0);
+    transform.Translate(0, 0, z);
+	}
+}
--- MyOwnTime/Test/Assets/scripts/Player.cs
+++ MyOwnTime/Test/Assets/scripts/Player.cs
@@ -3,35 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-	private Rigidbody rb;
-	private bool ismovingright = false;
+    private Rigidbody rb;
+    private bool ismovingright = false;
 
-	[SerializeField]
-	float speed = 4f;
-	void start ()	{
-		rb=this.GetComponent<Rigidbody>();
-	}
+    [SerializeField]
+    float speed = 4f;
+    void start () {
+        rb = this.GetComponent<Rigidbody> ();
+    }
 
-	void Update {
-		if(Input.GetMouseButtonDown(0))
-		{
-			ChangeBoolean();
-			ChangeDirection();
-		}
-	}
+    void Update () {
+        if (Input.GetMouseButtonDown (0)) {
+            ChangeBoolean ();
+            ChangeDirection ();
+        }
+    }
 
-	private void ChangeBoolean()
-	{
-		ismovingright = !ismovingright;
-	}
-	private void ChangeDirection()
-	{
-		if (ismovingright) 
-		{
-			rb.velocity = new Vector3(speed,0f,0f);
-		}else
-		{
-			rb.velocity = new Vector3(0f,0f,speed);
-		}
-	}
+    private void ChangeBoolean () {
+        ismovingright = !ismovingright;
+    }
+    private void ChangeDirection () {
+        if (ismovingright) {
+           rb.velocity = new Vector3(speed, 0f, 0f);
+        } else {
+             rb.velocity = new Vector3(0f, 0f, speed);
+        }
+    } /* code for movement
+    var x = Imput.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+    var z = Imput.GetAzis("Vertical") * Time.deltaTime * 3.0f
+    
+    transform.Rotate(0, x, );
+    transform.Translate(0, 0, z); */
 }
