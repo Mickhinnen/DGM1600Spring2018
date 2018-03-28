502469266aedb6f20162365b0152721a9ace0117
Unknown <mickailahinnen@gmail.com>
2018-03-28 14:20:27 -0600

    attempted speed powerup

---
+++ FirstGame/Assets/CharacterControler/MoveBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBase : MonoBehaviour {
+
+public float Speed;
+
+	void OnTriggerEnter(Collider other)
+	{
+		print ("hit " + other);
+		
+	}
+}
