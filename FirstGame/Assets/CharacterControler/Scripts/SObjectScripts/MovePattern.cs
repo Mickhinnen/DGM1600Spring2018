d2254493eaf90b0634d9eedb5b17e820ee02de1c
Unknown <mickailahinnen@gmail.com>
2018-04-03 11:14:39 -0600

    SCRIPTABLE OBJECTS

--- FirstGame/Assets/CharacterControler/Scripts/SObjectScripts/MovePattern.cs
+++ FirstGame/Assets/CharacterControler/Scripts/SObjectScripts/MovePattern.cs
@@ -9,15 +9,20 @@ public class MovePattern : ScriptableObject {
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
 	private Vector3 moveDirection = Vector3.zero;
+
+    public InputBase InputX;
+    public InputBase InputY;
+    public InputBase InputZ;
+
 public void Move (CharacterController controller, Transform Mytransform)
 {
 	 if (controller.isGrounded) {
-			moveDirection.x = Input.GetAxis("Horizontal");
-			moveDirection.z = Input.GetAxis("Vertical");
-			moveDirection.y = 0;
+			moveDirection.x = InputX.SetFloat();
+            moveDirection.y = InputY.SetFloat();
+			moveDirection.z = InputZ.SetFloat();
             moveDirection = Mytransform.TransformDirection(moveDirection);
             moveDirection *= speed;
-            if (Input.GetButton("Jump"))
+            if (Input.GetButton("Jump"))//bool.. boolean variable
                 moveDirection.y = jumpSpeed;
             
         }
---
+++ MyOwnTime/Test/Assets/scripts/ScriptableObjects/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Player : ScriptableObject {
+
+	public int Health;
+	public float Speed;
+	public float Jumpspeed;
+	public float gravity;
+	
+}
