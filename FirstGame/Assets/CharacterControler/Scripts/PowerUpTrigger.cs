33156dacea3407c200f80d3ef6d103b5f2015b6e
Unknown <mickailahinnen@gmail.com>
2018-04-05 11:30:55 -0600

    I HAVE LEARNED TRIGGER OBJECT

---
+++ FirstGame/Assets/CharacterControler/Scripts/PowerUpTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpTrigger : MonoBehaviour {
+
+	public PowerUp PowerUpTransfer;
+	//public MovePattern PowerUp;
+
+	//public GameObject pickupEffect;
+	
+
+   private void OnTriggerEnter(Collider obj)
+	{	
+		obj.GetComponent<MoveCharacter>().MovePattern = PowerUpTransfer.Transfer();
+
+		/*if (other.CompareTag("Player"))
+		{
+			StartCoroutine(Pickup(other));
+		}*/
+	}		
+	
+/*	IEnumerator Pickup(Collider Player)
+		{
+			print("You hit ");
+
+			Instantiate(pickupEffect, transform.position, transform.rotation);
+
+			//effect	
+
+			Destroy(gameObject);
+		}
+*/	
+
+}
---
+++ FirstGame/Assets/CharacterControler/Scripts/SObjectScripts/PowerUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PowerUp : ScriptableObject 
+{
+	public int PowerLevel;
+
+	public MovePattern MovePatternTransfer;
+
+	public MovePattern Transfer()
+	{
+		return MovePatternTransfer;
+	}
+	
+}
