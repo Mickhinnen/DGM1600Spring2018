3f18dfe6f120ba7083c3922cc4adafa350078896
Unknown <mickailahinnen@gmail.com>
2018-04-12 11:29:48 -0600

    Health Bar

--- FirstGame/Assets/CharacterControler/Scripts/PowerUpTrigger.cs
+++ FirstGame/Assets/CharacterControler/Scripts/PowerUpTrigger.cs
@@ -1,23 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerUpTrigger : MonoBehaviour {
 
 	public PowerUp PowerUpTransfer;
-	private MovePattern prevMovePattern;
+	public float PowerLevel = 0.2;
+	public Image HealthBar;
 	//public GameObject pickupEffect;
 	
 
    private void OnTriggerEnter(Collider obj)
 	{	
-		prevMovePattern = obj.GetComponent<MoveCharacter>().MovePattern;
+		HealthBar.FillAmount += PowerLevel;
 
-		obj.GetComponent<MoveCharacter>().MovePattern = PowerUpTransfer.Transfer();
+		if (HealthBar.FillAmount = 0);
+		{
+			obj.GetComponent<MoveCharacter>().MovePattern = PowerUpTransfer.Transfer();
 		
-
-		
-		gameObject.SetActive(false);
+			gameObject.SetActive(false);
+		}
 		/*if (other.CompareTag("Player"))
 		{
 			StartCoroutine(Pickup(other));
--- FirstGame/Assets/CharacterControler/Scripts/SObjectScripts/PowerUp.cs
+++ FirstGame/Assets/CharacterControler/Scripts/SObjectScripts/PowerUp.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 [CreateAssetMenu]
 public class PowerUp : ScriptableObject 
 {
-	public int PowerLevel;
 
 	public MovePattern MovePatternTransfer;
 
